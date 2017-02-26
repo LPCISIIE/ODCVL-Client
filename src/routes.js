28ee6b44766ab3ab6a9d225a7693f04584818030
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-26 21:43:14 +0100

    Changed middleware dir

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 
-import GuestMiddleware from './app/middleware/guest.middleware'
-// import AuthMiddleware from './app/middleware/auth.middleware'
+import GuestMiddleware from './middleware/guest.middleware'
+// import AuthMiddleware from './middleware/auth.middleware'
 
 export default function route ($stateProvider) {
   $stateProvider
