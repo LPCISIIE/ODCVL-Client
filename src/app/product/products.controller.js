a1294b918491d6c2d1eb621d9bf8801c1080646e
Alexis Wurth <alexis.wurth57@gmail.com>
2017-03-02 23:58:55 +0100

    Added search feature

--- src/app/product/products.controller.js
+++ src/app/product/products.controller.js
@@ -1,8 +1,33 @@
 
-export default function ProductsController ($scope, Product) {
+export default function ProductsController ($scope, Product, Category, CategoryProduct) {
+  $scope.category_id = 0
+
   Product.query(products => {
     $scope.products = products
   })
+
+  Category.query(categories => {
+    let allCategories = []
+    categories.forEach(category => {
+      allCategories.push(category)
+      category.sub_categories.forEach(subCategory => {
+        allCategories.push(subCategory)
+      })
+    })
+
+    allCategories.unshift({
+      id: 0,
+      name: 'Choisissez une catégorie'
+    })
+
+    $scope.categories = allCategories
+  })
+
+  $scope.filterProducts = () => {
+    CategoryProduct.query({ category_id: $scope.category_id }, products => {
+      $scope.products = products
+    })
+  }
 
   $scope.delete = (product) => {
     Product.delete(product, () => {
@@ -13,4 +38,4 @@ export default function ProductsController ($scope, Product) {
   }
 }
 
-ProductsController.$inject = ['$scope', 'Product']
+ProductsController.$inject = ['$scope', 'Product', 'Category', 'CategoryProduct']
