f1e556e8b1af02479b027d04d14c4da1055e683d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-03-06 20:45:10 +0100

    Update webpack.base.js

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -15,12 +15,12 @@ module.exports = {
     extensions: ['', '.js', '.css', '.html']
   },
   module: {
-    /* preLoaders: [{
+    preLoaders: [{
         test: /\.js$/,
         loader: 'eslint',
         exclude: /(node_modules|bower_components)/
       }
-    ], */
+    ],
     loaders: [{
         test: /\.s?css$/,
         loaders: ['style', 'css', 'sass']
