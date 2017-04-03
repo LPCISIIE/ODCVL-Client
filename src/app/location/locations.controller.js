bb55f9e6493c0b31910fb6c9f3ac8596ba2d7e9e
Zakaria <realandraja@gmail.com>
2017-04-03 02:19:02 +0200

    fixes

--- src/app/location/locations.controller.js
+++ src/app/location/locations.controller.js
@@ -1,6 +1,6 @@
 
 export default function LocationController ($scope, $state, $stateParams, Location, uiGridConstants, FlashService) {
-  const ITEMS_PER_PAGE = 20
+  const ITEMS_PER_PAGE = 7
 
   $scope.page = !isNaN($stateParams.page) ? parseInt($stateParams.page) : 1
   $scope.url = $state.href('locations.all', { page: '' })
